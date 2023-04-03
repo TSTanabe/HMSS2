a804c60145e1b93448e0f2f7df8346912176d9c9
SebastianTanabe <98402539+TSTanabe@users.noreply.github.com>
2023-04-03 21:18:54 +0200

    Add files via upload

---
+++ HMSSS.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+import sys
+from scripts.__main__ import main
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    main(args)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
