71d951e300aa4423434718f5f3d2a9ee7ee58535
TSTanabe <sebastian.tanabe@gmail.com>
2026-01-08 10:06:18 +0100

    debugging

--- src/hmsss/graft/read_models.py
+++ src/hmsss/graft/read_models.py
@@ -92,7 +92,7 @@ class Read:
 
         # build ungapped query from alignment
         query = "".join(c for c in self.alignment if c not in "-. \n\r\t")
-        start = self.sequence.find(query)
+        start = self.sequence.find(query[0:6])  # Workaround for less big seqs
         if start == -1:
             self.start = 0
         else:
